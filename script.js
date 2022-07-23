6a27268ead4ccb72bf74695cbd3b6989d472d1f1
DaniloIDS <danilo.ids@outlook.com>
2022-07-22 23:51:53 -0300

    Implement. Cont. Regressiva

--- script.js
+++ script.js
@@ -1,6 +1,4 @@
 
-
-
 btnCalc.addEventListener('click', contar)
 
 function contar(){
@@ -11,6 +9,7 @@ cFinal = document.getElementById('contFinal')
 step = document.getElementById('contStep')
 resultado = document.getElementById('resultado')
 resultado.innerHTML = ""
+let tpContagem;
 
   if (cInicial.value == "" || cFinal.value == ""){
     alert('Preencha os campos corretamente')
@@ -19,10 +18,27 @@ resultado.innerHTML = ""
     step.value = 1
   }
 
-  for(let i = Number(cInicial.value); i<= Number(cFinal.value); i = i + Number(step.value)){
-    i + Number(step.value) > cFinal.value ? 
-    resultado.innerHTML += i + "&#x1F6A9;" :
-    resultado.innerHTML += i + "&#x27A1;"    
+  Number(cInicial.value) <= Number(cFinal.value) ? tpContagem = "progressiva" : tpContagem = "regressiva"
+
+  switch (tpContagem) {
+    case "progressiva":
+
+      for(let i = Number(cInicial.value); i<= Number(cFinal.value); i = i + Number(step.value)){
+        i + Number(step.value) > cFinal.value ? 
+        resultado.innerHTML += i + "&#x1F6A9;" :
+        resultado.innerHTML += i + "&#x27A1;"    
+      }
+    break;
+
+    case "regressiva":
+    
+      for(let i = Number(cInicial.value); i >= Number(cFinal.value); i = i - Number(step.value)){
+        i - Number(step.value) < cFinal.value ? 
+        resultado.innerHTML += i + "&#x1F6A9;" :
+        resultado.innerHTML += i + "&#x27A1;"    
+      }
+    break;
+  
   }
 
   cInicial.value = ""
